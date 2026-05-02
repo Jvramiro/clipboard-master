b01051b48b1276e0e673f6a7b887235183b631dc
João Victor Ramiro <85127729+Jvramiro@users.noreply.github.com>
2026-05-01 22:16:15 -0300

    Feat: App Module

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,41 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ClipboardService } from './services/clipboard';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('clipboard-master');
+  statusMessage = '';
+
+  constructor(private clipboardService: ClipboardService) {}
+
+  async pasteClipboard() {
+    try {
+      const text = await navigator.clipboard.readText();
+      this.clipboardService.save(text).subscribe(() => {
+        this.statusMessage = 'Clipboard saved';
+      });
+    }
+    catch {
+      this.statusMessage = 'Clipboard not saved';
+    }
+  }
+
+  copyClipboard() {
+    this.clipboardService.get().subscribe(async (data) => {
+      if(data?.text){
+        await navigator.clipboard.writeText(data.text);
+      }
+      else{
+        this.statusMessage = 'There is no available data'
+      }
+    })
+  }
 }
